6c3019f23ca6d344c275b1c4a88968b58ff54ee3
Miguel Haba <mihaben89@gmail.com>
2024-07-25 14:59:22 +0200

    fix build export channels modules

--- src/channels/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseChannel } from "./base-channel.class";
-export { default as CookiesChannel } from "./cookies-channel.class";
-export { default as CustomEventChannel } from "./custom-event-channel.class";
-export { default as QueryParamsClientChannel } from "./query-params-client-channel.class";
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import featureFlags from "./feature-flags.class";
 
-export * from "./channels";
+// Channels
+export { default as BaseChannel } from "./channels/base-channel.class";
+export { default as CookiesChannel } from "./channels/cookies-channel.class";
+export { default as CustomEventChannel } from "./channels/custom-event-channel.class";
+export { default as QueryParamsClientChannel } from "./channels/query-params-client-channel.class";
 
+// Hook
 export { default as useFeatureFlag } from "./use-feature-flag";
 
 export default featureFlags;
